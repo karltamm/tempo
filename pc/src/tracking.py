8624fa48b9bdb6fb13d1f0ff90c58e805eaf80ef
adaresa <kasper.vaus0@gmail.com>
2021-09-12 17:23:04 +0300

    Create tracking.py
    added tracking class

---
+++ pc/src/tracking.py
@@ -0,0 +1,44 @@
+class Tracking:
+    """Class to track information about individual robots on the track"""
+    def __init__(self, name):
+        self.name = name
+        self.lap_times = {}
+        self.lap_id = 1
+        
+    def changeName(self, new_name):
+        self.name = new_name
+    
+    def addTime(self, time_ms):
+        self.lap_times[self.lap_id] = time_ms
+        self.lap_id += 1
+
+    def removeTime(self, time_id):
+        if time_id in self.lap_times.keys():
+            self.lap_times.pop(time_id)
+    
+    def printData(self):
+        print("Name of robot:", self.name)
+        if not self.lap_times: # empty list is False
+            print("No lap times have been added.")
+        else:
+            # a = sorted(self.lap_times.items(), key=lambda x: x[1]) # SORTS TIMES IN ASCENDING ORDER (FASTEST TIMES FIRST)
+            for key, value in self.lap_times.items():
+                print(key, ") ", value, "ms")              
+
+""" TRACKING TESTING """
+robot1 = Tracking("Terminaator")
+robot1.addTime(77123)
+robot1.addTime(82000)
+robot1.addTime(44555)
+robot1.addTime(65234)
+robot1.removeTime(2)
+robot1.removeTime(7)
+
+robot2 = Tracking("ERROR")
+robot2.changeName("WALL-E")
+
+robots = [robot1, robot2]
+for robot in robots:
+    robot.printData()
+    print("")
+""" TESTING END """
