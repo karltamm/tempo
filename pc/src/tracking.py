c93025f03d5ec83e7403d7b7f52de2a526a13c7d
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-09-20 18:00:22 +0300

    Tracking class getData()

--- pc/src/tracking.py
+++ pc/src/tracking.py
@@ -1,13 +1,14 @@
 class Tracking:
     """Class to track information about individual robots on the track"""
+
     def __init__(self, name):
         self.name = name
         self.lap_times = {}
         self.lap_id = 1
-        
+
     def changeName(self, new_name):
         self.name = new_name
-    
+
     def addTime(self, time_ms):
         self.lap_times[self.lap_id] = time_ms
         self.lap_id += 1
@@ -15,15 +16,19 @@ class Tracking:
     def removeTime(self, time_id):
         if time_id in self.lap_times.keys():
             self.lap_times.pop(time_id)
-    
+
     def printData(self):
         print("Name of robot:", self.name)
-        if not self.lap_times: # empty list is False
+        if not self.lap_times:  # empty list is False
             print("No lap times have been added.")
         else:
             # a = sorted(self.lap_times.items(), key=lambda x: x[1]) # SORTS TIMES IN ASCENDING ORDER (FASTEST TIMES FIRST)
             for key, value in self.lap_times.items():
-                print(key, ") ", value, "ms")              
+                print(key, ") ", value, "ms")
+
+    def getData(self):
+        return {"robot_name": self.name, "lap_times": self.lap_times}
+
 
 """ TRACKING TESTING """
 robot1 = Tracking("Terminaator")
@@ -41,4 +46,6 @@ robots = [robot1, robot2]
 for robot in robots:
     robot.printData()
     print("")
+
+print(f"\n\nDATA: {robot1.getData()}")
 """ TESTING END """
