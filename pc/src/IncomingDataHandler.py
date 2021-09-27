6ed34b9b16f8cd2b3ed6d1fc0e57c6f42db2ff4a
adaresa <kasper.vaus0@gmail.com>
2021-09-27 18:31:07 +0300

    Update IncomingDataHandler.py

--- pc/src/IncomingDataHandler.py
+++ pc/src/IncomingDataHandler.py
@@ -1,7 +1,14 @@
 import serial
+import serial.tools.list_ports
 
-arduino = serial.Serial('dev/ttyACM0', timeout=5)
+arduino_ports = [
+    p.device
+    for p in serial.tools.list_ports.comports()
+    if 'Arduino' in p.description  # may need tweaking to match new arduinos
+] # All ports with arduino connected added to list
+
+ser = serial.Serial(arduino_ports[0]) # Open serial port with first arduino in list
 
 data = []
-data.append(str(arduino.readline()))
+data.append(str(ser.readline())) # using ser.readline() assumes each line contains a single reading, sent using Serial.println() on the Arduino
 print(data)
